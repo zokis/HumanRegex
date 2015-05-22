2ed9cf542d46c86c0332b480dc4ccea53429b3ad
Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>
2015-05-22 20:10:15 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name='hre',
+    version='0.1.0',
+    description='helper for manipulating regex.',
+    url='https://github.com/zokis/HumanRegex/',
+    author='Marcelo Fonseca Tambalo',
+    author_email='marcelo.zokis@gmail.com',
+    license='WTFL',
+    py_modules=['hre'],
+	scripts=['hre.py'],
+	platforms = 'any',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
